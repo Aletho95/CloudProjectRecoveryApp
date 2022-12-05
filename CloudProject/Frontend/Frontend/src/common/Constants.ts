88203fed06f4285519aef4885d5d6fb641e8261e
Aletho95 <altho18@student.sdu.dk>
2022-12-06 00:28:18 +0100

    change some configs, now it works

--- CloudProject/Frontend/Frontend/src/common/Constants.ts
+++ CloudProject/Frontend/Frontend/src/common/Constants.ts
@@ -1,5 +1,7 @@
 const constansts = {
-    serverBaseURL: (process.env.NODE_ENV === 'production') ? 'https://iob.news:3000' : 'http://localhost:3000' //TODO: If deploying to heroku, change URL
+    serverBaseURL: (process.env.NODE_ENV === 'production') ? 'https://iob.news:3000' : 'https://cloud-project-recovery-app-backend-4yyqmoxy6a-uc.a.run.app' //TODO: If deploying to heroku, change URL
 }
 
 export default constansts;
+
+//serverBaseURL: (process.env.NODE_ENV === 'production') ? 'https://iob.news:3000' : 'http://localhost:3000' //TODO: If deploying to heroku, change URL
